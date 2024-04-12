a522d8678cd6423fe3f107432dfe8c4312c7f02e
[Aman Yadav] <[aman4864y@gmail.com]>
2024-04-13 01:41:24 +0530

    Add sidebar and its animation

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,25 +1,37 @@
-import "./navbar.scss"
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
-const Navbar = ()=> {
-    return (
-    
-        <div className="navbar">
-        {/* {sidebar} */}
-        <div className="wrapper">
-            <span>Aman Yadav</span>
-            <div className="social">
-                <a href="#"><img src="/public/facebook.png" alt=""></img></a>
-                <a href="#"><img src="/public/instagram.png" alt=""></img></a>
-                <a href="#"><img src="/public/youtube.png" alt=""></img></a>
-                <a href="#"><img src="public/dribbble.png" alt=""/></a>
-
-            </div>
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Aman Yadav
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
         </div>
-        </div>
+      </div>
+    </div>
+  );
+};
 
-
-
-    )
-}
-
-export default Navbar
+export default Navbar;
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import "./app.scss"
 import Navbar from "../components/Navbar/Navbar";
+import Test from "./Test.jsx"
+
+
 
 const App = () => {
   return <div>
-    <section>
+    <section id="HomePage">
       <Navbar/>
     </section>
-    <section>Parallax</section>
+    
+    <section id="Services">Parallax</section>
     <section>Services</section>
-    <section>Parallax</section>
+    <section id="Portfolio">Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section>
+    <section id=" contact">Contact</section>
+    {/* <Test/>
+    <Test/> */}
+    
   </div>;
+
 };
 
 export default App;
